bd93d77c78d83b677e7d5c47fc618d10d7437d19
manish <manish.chaugule@infojiniconsulting.com>
2020-09-19 18:21:24 +0530

    final push

--- src/screen/home/component/AllTable/index.js
+++ src/screen/home/component/AllTable/index.js
@@ -9,56 +9,59 @@ export default function AllTable ({detail , onShortlistClick , onDeleteClick}) {
                 ? <div className ="body-div">
                     NO DATA FOUND
                 </div>
-                :   <table>
-                        <thead>
-                            <tr className="table-header">
-                                <th> State </th>
-                                <th> District </th>
-                                <th> City </th>
-                                <th> Action </th>
-                            </tr>
-                        </thead>
-                            {
-                                detail.map((value , stateIndex)=>{
-                                    return(
-                                        <tbody key ={stateIndex}>
-                                            <tr className="table-body" >
-                                                <td>
-                                                    {value.State}
-                                                </td>
-                                                <td>
-                                                    {value.District}
-                                                </td>
-                                                <td>
-                                                    {value.City}
-                                                </td>
-                                                <td >
-                                                    {
-                                                        !value.isSelected
-                                                        ? <div className = "button-container">
-                                                            <div className= "button"
-                                                                onClick = {_ => { onShortlistClick(value.id)}}> 
-                                                                Shortlist 
+                :  <div className="table-container">
+                        <table>
+                            <thead>
+                                <tr className="table-header">
+                                    <th> State </th>
+                                    <th> District </th>
+                                    <th> City </th>
+                                    <th> Action </th>
+                                </tr>
+                            </thead>
+                                {
+                                    detail.map((value , stateIndex)=>{
+                                        return(
+                                            <tbody key ={stateIndex}>
+                                                <tr className="table-body" >
+                                                    <td>
+                                                        {value.State}
+                                                    </td>
+                                                    <td>
+                                                        {value.District}
+                                                    </td>
+                                                    <td>
+                                                        {value.City}
+                                                    </td>
+                                                    <td >
+                                                        {
+                                                            !value.isSelected
+                                                            ? <div className = "button-container">
+                                                                <div className= "button"
+                                                                    onClick = {_ => { onShortlistClick(value.id)}}> 
+                                                                    Shortlist 
+                                                                </div>
+                                                                <div className= "button delete-color" 
+                                                                    onClick = {_ => {onDeleteClick(value.id)}}> 
+                                                                    Delete 
+                                                                </div>
                                                             </div>
-                                                            <div className= "button delete-color" 
-                                                                onClick = {_ => {onDeleteClick(value.id)}}> 
-                                                                Delete 
+                                                            :<div className ="action-buttons">
+                                                                <div className= "button delete-color" 
+                                                                    onClick = {_ => {onDeleteClick(value.id)}}> 
+                                                                    Delete 
+                                                                </div>
                                                             </div>
-                                                        </div>
-                                                        :<div className ="action-buttons">
-                                                            <div className= "button delete-color" 
-                                                                onClick = {_ => {onDeleteClick(value.id)}}> 
-                                                                Delete 
-                                                            </div>
-                                                        </div>
-                                                    }
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    )
-                                })
-                            }
-                    </table>
+                                                        }
+                                                    </td>
+                                                </tr>
+                                            </tbody>
+                                        )
+                                    })
+                                }
+                        </table>
+                    </div>
+                 
             }
         </div>
     )
